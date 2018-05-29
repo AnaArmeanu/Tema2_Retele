b35cb440d144ed6e6aa8139f96fabc8cc325afd1
AnaArmeanu <32650626+AnaArmeanu@users.noreply.github.com>
2018-05-29 10:04:19 +0300

    Add files via upload

--- Ex2_ARP/ex2ARP.py
+++ Ex2_ARP/ex2ARP.py
@@ -2,15 +2,11 @@
 from scapy.all import *
  
 eth = Ether(dst = "ff:ff:ff:ff:ff:ff")
-#arp = ARP(pdst = '198.13.13.1')#pdst = ip destinatie
-arp = ARP(pdst = "198.13.13.0/16")
-#subnetul lui net
-answered,unanswered = srp(eth / arp, timeout = 2, inter = 0.1 )
+arp = ARP(pdst = "198.13.13.0/16")#subnet net
+ans, unans = srp(eth / arp)
 #srp trimite pachete in retea si primeste raspunsuri (protocoale pentru pachete: Ethernet si ARP)
-#inter = timp de asteptare intre pachete - 0.1 secunde
-#timeout = timp asteptare confirmare
 print "IP -- MAC\n"
-for snd,rcv in answered:
+for snd,rcv in ans:
     print rcv[ARP].psrc + " -- " + rcv[Ether].src
 
     
